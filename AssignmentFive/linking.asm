e454125dac3e246e66f0bc52f43e5d572d19b272
prashathc <prashathc@gmail.com>
2020-11-22 21:30:22 -0500

    still wokring on assignment 5

--- AssignmentFive/linking.asm
+++ AssignmentFive/linking.asm
@@ -7,20 +7,41 @@
 INCLUDE Irvine32.inc
 
 .data
+prompt		BYTE	"Please enter the name of the data file: ", 0
+fileName	BYTE	20 DUP(?)
+fileHandle	DWORD	?
+msgLength	DWORD	?
 letters		BYTE	256 DUP(?)
 index		BYTE	256 DUP(?)
-prompt		BYTE	"Please enter the name of the data file: ", 0
 
 
 .code
-	mov edx, OFFSET prompt
-	Call WriteString
-
-
-
-
 main PROC
-	; write your code here
+	mov edx, OFFSET prompt
+	call WriteString
+
+	mov edx, OFFSET fileName
+	mov ecx, SIZEOF fileName
+	call ReadString					;save file name into edx
+
+
+	mov edx, OFFSET fileName
+	call OpenInputFile				;open file using file name from edx
+	mov fileHandle, eax				;save file handle
+
+	mov eax, fileHandle
+	mov edx, OFFSET letters 
+	mov ecx, SIZEOF msgLength
+	call ReadFromFile
+
+
+
+
+
+	
+
+	call CloseFile
+	
 	exit
 main ENDP
 ; (insert additional procedures here)
