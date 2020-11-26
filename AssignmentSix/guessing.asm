1152b9ba3148b3599f9258652913d0487fd8caac
prashathc <prashathc@gmail.com>
2020-11-26 14:58:10 -0500

    starting assignment 6

---
+++ AssignmentSix/guessing.asm
@@ -0,0 +1,20 @@
+; Program Template		(Temple.asm)
+
+; Program Description:
+; Author:
+; Creation Date:
+; Revisions:
+; Date:				Modified by:
+
+INCLUDE Irvine32.inc
+
+.data
+; declare variables here
+.code
+
+main PROC
+	; write your code here
+	exit
+main ENDP
+; (insert additional procedures here)
+END main
